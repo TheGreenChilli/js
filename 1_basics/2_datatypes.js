a75363fda93b4ce779f70670bf7c1291a895c8ff
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-08-26 09:55:41 +0000

    datatypes

--- 1_basics/2_datatypes.js
+++ 1_basics/2_datatypes.js
@@ -7,3 +7,20 @@ console.log(3
     3) // code readability should be high
     
 console.log("sanjana")
+
+let name = "sanjana";
+let age = 21;
+let isloggedIn = true;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
