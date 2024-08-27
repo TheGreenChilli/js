29ee04778914ece58b660857967733556953e4e3
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-08-27 18:55:06 +0000

    strings

--- 1_basics/5_strings.js
+++ 1_basics/5_strings.js
@@ -4,3 +4,28 @@ const repoCount = 50;
 //console.log(name + repoCount + "Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  // String Interpolation
+
+
+const gameName = new String("sanj-anak");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("s"));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStringOne = "  sanjana  ";
+console.log(newStringOne);
+console.log(newString.trim());
+
+const url = "https://sanjana.com/sanjana%20dangi";
+
+console.log(url.replace("%20", "-"));
