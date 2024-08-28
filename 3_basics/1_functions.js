f3bbee5e660034f17faf09da22b452695dc8317a
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-08-28 14:25:55 +0000

    functions

---
+++ 3_basics/1_functions.js
@@ -0,0 +1,68 @@
+function sayMyName() {
+    console.log("s");
+    console.log("a");
+    console.log("n");
+    console.log("j");
+    console.log("a");
+    console.log("n");
+    console.log("a");
+  }
+  
+  //sayMyName();
+  
+  // function addTwoNumbers(number1, number2) {
+  //   console.log(number1 + number2);
+  // }
+  
+  function addTwoNumbers(number1, number2) {
+    // let result = number1 + number2;
+    // return result;
+    return number1 + number2;
+  }
+  
+  const result = addTwoNumbers(3, 5);
+  
+  // console.log("Result: ", result);
+  
+  function loginUserMessage(username = "sanju") {
+    if (!username) {
+      console.log("Please enter a username");
+      return;
+    }
+    return `${username} just logged in`;
+  }
+  
+  // console.log(loginUserMessage("sanjana"));
+  // console.log(loginUserMessage("sanjana"));
+  
+  function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+  
+  }
+  
+  // console.log(calculateCartPrice(200, 400, 500, 2000))
+  
+  const user = {
+    username: "sanju",
+    price: 199
+  }
+  
+  function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+  }
+  
+  // handleObject(user)
+  handleObject({
+    username: "sam",
+    price: 299
+  })
+  
+  const myNewArray = [200, 400, 100, 600]
+  
+  function returnSecondValue(getArray) {
+    return getArray[1]
+  }
+  
+  // console.log(returnSecondValue(myNewArray));
+  console.log(returnSecondValue([200, 400, 500, 1000])); 
+  
