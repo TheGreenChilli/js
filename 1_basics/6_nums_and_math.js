8bc6d654b5547c3ddad4bdb8682ff4e45222a8fd
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-08-28 06:04:54 +0000

    nums and math

--- 1_basics/6_nums_and_math.js
+++ 1_basics/6_nums_and_math.js
@@ -14,21 +14,22 @@
 const hundreds = 1000000;
 // console.log(hundreds.toLocaleString("en-IN"));
 
+
 // ************ Maths **************
 
 // console.log(Math);
-// console.log(Math.abs(-4));
+// console.log(Math.abs(-4));  
 // console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.2));           // upper value
+// console.log(Math.floor(4.9));          // lower value
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random()); //0-1
-console.log(Math.random() * 10 + 1);
-console.log(Math.floor(randomMath.random() * 10) + 1); // 1-9
+// console.log(Math.random()); //0-1
+// console.log(Math.random() * 10 + 1);
+// console.log(Math.floor(randomMath.random() * 10) + 1); // 1-9
 
-const min = 10;
-const max = 20;
+// const min = 10;
+// const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
