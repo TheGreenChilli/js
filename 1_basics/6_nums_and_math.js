5b2abf0ee3c3449106b1850876e42c702cd67a98
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-08-28 06:09:19 +0000

    nums and math

--- 1_basics/6_nums_and_math.js
+++ 1_basics/6_nums_and_math.js
@@ -25,9 +25,9 @@ const hundreds = 1000000;
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-// console.log(Math.random()); //0-1
+// console.log(Math.random());           //0-1
 // console.log(Math.random() * 10 + 1);
-// console.log(Math.floor(randomMath.random() * 10) + 1); // 1-9
+// console.log(Math.floor(Math.random() * 10) + 1); // 1-9
 
 // const min = 10;
 // const max = 20;
