7d2fa364c4da2b76f049054eceee610807bb4c4a
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-09-04 11:48:49 +0000

    one

---
+++ 4_control_flow.js/one.js
@@ -0,0 +1,11 @@
+// if
+
+// single assignment(=) is used for assignment operator, not for comparison
+
+const isUserLoggedIn = true
+const temperature = 41
+
+if ( temperature === 41){
+    console.log("eless than 50");
+}
+console.log("temperature is greater than 50");
