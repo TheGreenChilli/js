9bed5cbb49f342192ac933ec172246f095d3e7d8
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-09-04 15:08:36 +0000

    one

--- 4_control_flow/one.js
+++ 4_control_flow/one.js
@@ -2,12 +2,54 @@
 
 // single assignment(=) is used for assignment operator, not for comparison
 
-const isUserLoggedIn = true
-const temperature = 41
+// const isUserLoggedIn = true
+// const temperature = 41
 
-if ( temperature === 41){
-    console.log("less than 50");
-} else { 
-    console.log("temperature is greater than 50");
+// if ( temperature === 41){
+//     console.log("less than 50");
+// } else { 
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Execute");
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+// ***************** SHORTHAND NOTATION ************
+
+const balance = 1000
+
+// if (balance > 500) console.log("test");      // implicit scope 
+
+// if (balance > 500) console.log("test"), console.log("test2");   // Don't code like this
+
+
+// if (balance > 500) {
+//     console.log("less than");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// }else if (balance < 900) {
+//     console.log("less than 750");
+// } else {
+//     console.log("less than 1200")
+// }
+
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFRomEmail = true
+
+if (userLoggedIn && debitcard &&  2==2) {
+    console.log("Allowed to buy course");
 }
-console.log("Execute");
+
+if (loggedInFromGoogle || loggedInFRomEmail) {
+    console.log("User logged in");
+}
