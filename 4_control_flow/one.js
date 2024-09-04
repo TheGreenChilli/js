607e696338425a1b392db1ea49b5a8a104cbb1e7
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-09-04 14:47:48 +0000

    one

--- 4_control_flow/one.js
+++ 4_control_flow/one.js
@@ -6,6 +6,8 @@ const isUserLoggedIn = true
 const temperature = 41
 
 if ( temperature === 41){
-    console.log("eless than 50");
+    console.log("less than 50");
+} else { 
+    console.log("temperature is greater than 50");
 }
-console.log("temperature is greater than 50");
+console.log("Execute");
