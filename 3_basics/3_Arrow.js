28e547eafd75465771df274b744e745e390bb4f8
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-09-04 10:24:34 +0000

    Arrow

--- 3_basics/3_Arrow.js
+++ 3_basics/3_Arrow.js
@@ -11,10 +11,49 @@ const user = {
 
 // user.welcomeMessage()
 // user.username = "sanju"
-// user.welcomeMessage()
-
+// user.welcomeMessage()   
 // console.log(this);
 
 
+// function chai() {
+//     let username = "sanjana"
+//     console.log(this.username);
+// }
+
+// chai()
 
 
+// const chai = function() {
+//     let username = "sanjana"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "sanjana"
+    console.log(this);
+}
+
+
+// chai()
+
+
+// ****************** BASIC ARROW FUNCTION ***************
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// ******************** IMPLICIT RETURN *********************
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "sanjana"})   // object return
+
+console.log(addTwo(3, 4))
+
+
+const myArray = [1, 2, 3, 4, 5]
+
+myArray.forEach()
