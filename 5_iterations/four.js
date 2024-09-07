f2b1a35239e8f23690c129a2cabc6f039537b081
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-09-07 15:33:01 +0000

    four

---
+++ 5_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+  js: 'javascript',
+  cpp: 'C++',
+  rb: 'ruby',
+  swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+  console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ['javascript', 'C++', 'ruby', 'python', 'swift by apple']
+
+for (const key in programming) {
+ // console.log(programming[key]);
+}
+
+// const map = new Map() 
+// map.set('IN', "India") 
+// map.set('US', "United States")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//   console.log(key);
+// }
+
