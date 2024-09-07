20e68e7fe9a7b870145d236fcdc47bb763d996d4
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-09-07 09:08:36 +0000

    truthy

--- 4_control_flow/truthy.js
+++ 4_control_flow/truthy.js
@@ -25,5 +25,23 @@ if (userEmail.length === 0) {
 const emptyObj = {}
 
 if (Object.keys(emptyObj).length === 0) {
-  console.log("object is empty")
-}
+  console.log("object is empty");
+} 
+
+
+// Nullish Coslescing Operator (??): null undefined
+
+let val1;
+// val1 = 5  ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
