2e270a421d72b363165e8648741970ba7c624c0c
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-10-02 15:24:08 +0000

    BMI Calculator

--- 8_project_2/script.js
+++ 8_project_2/script.js
@@ -8,25 +8,12 @@ form.addEventListener('submit', function(e) {
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
 
-  // if (height === '' || height < 0 || isNaN(height)) {
-  //   results.innerHTML = `Please give a valid height ${height}`;
-  // }
-  // else if (weight === '' || weight < 0 || isNaN(weight)) {
-  //   results.innerHTML = `Please give a valid weight ${weight}`;
-  // } else {
-  //   const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-  //   results.innerHTML = `<span>${bmi}</span>`;
-  // }
-
-  if (isNaN(height) || height <= 0) {
-    results.innerHTML = `Please give a valid height`;
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
   }
-  // Check for valid weight
-  else if (isNaN(weight) || weight <= 0) {
-    results.innerHTML = `Please give a valid weight`;
-  } 
-  // Calculate BMI if inputs are valid
-  else {
+  else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     results.innerHTML = `<span>${bmi}</span>`;
   }
