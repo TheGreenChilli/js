c6bda6854276162bb10a939c0fd2a56c88da331c
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-10-03 12:41:42 +0000

    Guess the Number

---
+++ 10_project_4/script.js
@@ -0,0 +1,6 @@
+const randomNumber = parseInt(math.random() * 100 + 1);
+
+const submit = document.querySelector('subt');
+const input = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
