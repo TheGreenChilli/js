d3671f7a55c676f341996fd427253dad1af8248c
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-10-05 17:40:46 +0000

    oop

--- 10_classes_and_oops/oop.js
+++ 10_classes_and_oops/oop.js
@@ -1,13 +1,35 @@
+// object literal
+
 const user = {
      username: "sanju",
      loginCount: 8,
      signedIn: true,
 
      getUserDetails: function(){
-     console.log("Got user details form database");
+     //console.log("Got user details form database");
+     // console.log(`Username: ${this.username}`);
+     console.log(this);
      }
 
 }
 
-console.log(user.username);
-console.log(user.getUserDetails());
+//console.log(user.username);
+//console.log(user.getUserDetails());
+//console.log(this);
+
+function User(username, loginCount, isLoggedIn){
+     this.username = username;
+     this.loginCount = loginCount;
+     this.isLoggedIn = isLoggedIn;
+
+     this.greeting = function(){
+          console.log(`Hello ${this.username}`);
+     }
+
+     return this
+}
+
+const userOne =  new User("sanju", 8, true);
+const userTwo =  new User("sanvi", 12, false);
+console.log(userOne.constructor);
+//console.log(userTwo);
