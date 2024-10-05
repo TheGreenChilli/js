d018f9f7d7b90c5458fa68966d143ec50a7fd6a0
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-10-05 15:17:00 +0000

    promises

--- 9_advance_one/promises.js
+++ 9_advance_one/promises.js
@@ -65,5 +65,39 @@ const promiseFive = new Promise(function(resolve, reject){
   }, 1000)
 });
 
+
+async function consumePromiseFive(){
+  try{
+    const response = await promiseFive
+     console.log(response);
+  } catch (error) {
+     console.log(error);
+  }
+}
+
+consumePromiseFive()
+
+
+// async function getAllUsers(){
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data);
+//   } catch  (error) {
+//     console.log("E: ",error);
+//   }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+  response.json()
+})
+.then((data) => {
+  console.log(data);
+})
+.catch((error) => console.log(error))
+
  
 
