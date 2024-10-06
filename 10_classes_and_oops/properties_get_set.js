44646673a58e2e773246c5b3bb8ef22e9532109f
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-10-06 09:40:18 +0000

    properties get set

---
+++ 10_classes_and_oops/properties_get_set.js
@@ -0,0 +1,23 @@
+function User(email, password){
+  this._email = email;
+  this._pasasword = password
+
+  Object.defineProperty(this, 'email', {
+    get: function(){
+      return this._email.toUpperCase()
+    },
+    set: function(value){
+      this._email = value
+    })
+    Object.defineProperty(this, 'password', {
+      get: function(){
+        return this._pasasword.toUpperCase()
+      },
+      set: function(value){
+        this.password = value
+    }
+  })
+}
+
+const hey = new User('test@example.com', 'hey')
+console.log(hey.email);
